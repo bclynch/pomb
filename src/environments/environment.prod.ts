ef2d9b02c5c10c1c22026886bb9f6bbbbacee53c
bclynch <bclynch7@gmail.com>
2020-06-08 11:20:29 -0400

    add envs to gitignore

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const ENV = {
-  production: true,
-  googleAPIKey: 'AIzaSyAa8icfucqEezbxr0iAHg5sXaY2HbyOS2E',
-  flickrAPIKey: '691be9c5a38900c0249854a28a319e2c',
-  apolloBaseURL: '/api/graphql',
-  apiBaseURL: '/api',
-  disqusShortname: 'packonmyback'
-};
